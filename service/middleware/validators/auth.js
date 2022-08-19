598966315423b639b9256d67cff8fcf66f664876
kirinico <kirinico@gmail.com>
2022-08-19 19:32:14 -0300

    added more functionality to express validations

--- service/middleware/validators/auth.js
+++ service/middleware/validators/auth.js
@@ -2,11 +2,11 @@ const { check } = require("express-validator");
 const { validateResult } = require("../../data/validateResult");
 
 const validateRegister = [
-  check("firstName").exists().notEmpty(),
+  check("firstName").exists().notEmpty().isLength({min: 3, max: 20}).withMessage("el nombre tiene que ser de 3 a 20 caracteres!"),
   check("lastName").exists().notEmpty(),
-  check('email', 'agrega un email valido').isEmail(),
-  check('password', 'el password debe ser minimo de 6 caracteres').isLength({min: 6}),
-  check("dni").exists().notEmpty(),
+  check('email', 'agrega un email valido').isEmail().exists(),
+  check('password', 'el password debe ser minimo de 6 caracteres').isLength({min: 6}).exists(),
+  check("dni").exists().notEmpty().isNumeric(),
   check("cp").exists().notEmpty(),
   check("phone").exists().notEmpty(),
   check("alternativePhone").exists().notEmpty(),
--- service/middleware/validators/cart.js
+++ service/middleware/validators/cart.js
@@ -2,7 +2,7 @@ const { check } = require("express-validator");
 const { validateResult } = require("../../data/validateResult");
 const validateGenerateOrder = [
   check("user").exists().notEmpty(),
-  check("products").exists().notEmpty(),
+  check("products").exists().notEmpty().isArray(),
   check("total").exists().notEmpty(),
   (req, res, next) => {
     validateResult (req, res, next);
